81ac910f2f02a22aff0a339aff7ff63b6a28f135
Leann Fraoigh <d.myshlennikova@gmail.com>
2024-09-12 13:57:46 +0300

    fix: Import only necessaty fonts

--- src/main.tsx
+++ src/main.tsx
@@ -10,10 +10,8 @@ import {
 
 // Стили
 import './assets/css/index.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/cyrillic-400.css'
+import '@fontsource/roboto/cyrillic-500.css'
 import { CssBaseline } from '@mui/material';
 
 // Экраны приложения и их лоадеры
